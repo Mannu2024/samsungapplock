bf8c0d2fa75cd8587057718a30f17847ab521968
Manish Thakur <manishthakur2024@ramjas.du.ac.in>
2026-01-18 23:57:28 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'applock-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/store.js',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
